e0d3b5f3c37c86dfa4762f7cf593a1befd2da188
Andrius Jukna <aj@platform-lunar.com>
2019-11-11 23:03:56 +0200

    Split logic into update Quality & update SellIn

--- src/main/java/com/gildedrose/ItemWrapper.java
+++ src/main/java/com/gildedrose/ItemWrapper.java
@@ -9,6 +9,13 @@ class ItemWrapper {
     }
 
     void decreaseSellInDays() {
+        if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
+            item.sellIn = item.sellIn - 1;
+        }
+        updateQuality();
+    }
+
+    private void updateQuality() {
         if (!item.name.equals("Aged Brie") && !item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
             if (item.quality > 0) {
                 if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
@@ -20,13 +27,13 @@ class ItemWrapper {
                 item.quality = item.quality + 1;
 
                 if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                    if (item.sellIn < 11) {
+                    if (item.sellIn < 10) {
                         if (item.quality < 50) {
                             item.quality = item.quality + 1;
                         }
                     }
 
-                    if (item.sellIn < 6) {
+                    if (item.sellIn < 5) {
                         if (item.quality < 50) {
                             item.quality = item.quality + 1;
                         }
@@ -35,15 +42,6 @@ class ItemWrapper {
             }
         }
 
-
-
-        if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-            item.sellIn = item.sellIn - 1;
-        }
-        updateQuality();
-    }
-
-    private void updateQuality() {
         if (item.sellIn < 0) {
             if (!item.name.equals("Aged Brie")) {
                 if (!item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
