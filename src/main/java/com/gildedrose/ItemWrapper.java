2645b61ee1711d8069513565cd4610cbedd93b42
Andrius Jukna <aj@platform-lunar.com>
2019-11-11 23:07:57 +0200

    Use constants instead of hardcoded strings

--- src/main/java/com/gildedrose/ItemWrapper.java
+++ src/main/java/com/gildedrose/ItemWrapper.java
@@ -2,6 +2,10 @@ package com.gildedrose;
 
 class ItemWrapper {
 
+    private static final String SULFURAS = "Sulfuras, Hand of Ragnaros";
+    private static final String AGED_BRIE = "Aged Brie";
+    private static final String BACKSTAGE_PASSES = "Backstage passes to a TAFKAL80ETC concert";
+
     private final Item item;
 
     ItemWrapper(Item item) {
@@ -9,16 +13,16 @@ class ItemWrapper {
     }
 
     void decreaseSellInDays() {
-        if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
+        if (!item.name.equals(SULFURAS)) {
             item.sellIn = item.sellIn - 1;
         }
         updateQuality();
     }
 
     private void updateQuality() {
-        if (!item.name.equals("Aged Brie") && !item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+        if (!item.name.equals(AGED_BRIE) && !item.name.equals(BACKSTAGE_PASSES)) {
             if (item.quality > 0) {
-                if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
+                if (!item.name.equals(SULFURAS)) {
                     item.quality = item.quality - 1;
                 }
             }
@@ -26,7 +30,7 @@ class ItemWrapper {
             if (item.quality < 50) {
                 item.quality = item.quality + 1;
 
-                if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                if (item.name.equals(BACKSTAGE_PASSES)) {
                     if (item.sellIn < 10) {
                         if (item.quality < 50) {
                             item.quality = item.quality + 1;
@@ -43,10 +47,10 @@ class ItemWrapper {
         }
 
         if (item.sellIn < 0) {
-            if (!item.name.equals("Aged Brie")) {
-                if (!item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+            if (!item.name.equals(AGED_BRIE)) {
+                if (!item.name.equals(BACKSTAGE_PASSES)) {
                     if (item.quality > 0) {
-                        if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
+                        if (!item.name.equals(SULFURAS)) {
                             item.quality = item.quality - 1;
                         }
                     }
